210b697ecbce7c875a79ec01e22449c1523ecbe1
Adrian Cole <adrian@tetrate.io>
2024-02-22 08:57:56 +0800

    deps: updates to go 1.22
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- Tools.mk
+++ Tools.mk
@@ -1,7 +1,7 @@
 # Copyright 2021 Tetrate
 # Licensed under the Apache License, Version 2.0 (the "License")
 
-gofumpt       := mvdan.cc/gofumpt@v0.5.0
-golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+gofumpt       := mvdan.cc/gofumpt@v0.6.0
+golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.56.2
 gosimports    := github.com/rinchsan/gosimports/cmd/gosimports@v0.3.8
 licenser      := github.com/liamawhite/licenser@v0.7.0
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/tetratelabs/car
 
-go 1.19
+go 1.20
 
-require github.com/stretchr/testify v1.8.2
+require github.com/stretchr/testify v1.8.4
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
